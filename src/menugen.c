10c1201ac36520e8be3d0cbf6f828796632d5e21
Josh Pitkofsky <pitkofskyj@gmail.com>
2015-07-18 22:34:18 -0700

    added menu function

--- src/menugen.c
+++ src/menugen.c
@@ -1,10 +1,9 @@
-/*
- * main.c
- * Sets up Window, AppMessage and a TextLayer to show the message received.
- */
-
 #include <pebble.h>
-#define KEY_DATA 5
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
 #define NUM_MENU_SECTIONS 2
 #define NUM_MENU_ICONS 3
 #define NUM_FIRST_MENU_ITEMS 2
@@ -20,7 +19,7 @@ typedef struct {
   char *content;
 } article;
 
-article list[10];
+article list[2];
 
 //static int s_current_icon = 0;
 
@@ -76,9 +75,6 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
 }
 
 static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
-  
-  //SEND INDEX BACK
-  
   printf("FIRST");
   // Use the row to specify which item will receive the select action
   if(cell_index->section == 0) {
@@ -156,83 +152,33 @@ static void main_window_unload(Window *window) {
   gbitmap_destroy(s_background_bitmap);
 }
 
-static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  // Get the first pair
-  Tuple *t = dict_read_first(iterator);
-
-  // Process all pairs present
-  while (t != NULL) {
-    // Long lived buffer
-    static char s_buffer[64];
-
-    // Process this pair's key
-    switch (t->key) {
-      case KEY_DATA:
-        // Copy value and display
-        snprintf(s_buffer, sizeof(s_buffer), "Received '%s'", t->value->cstring);
-        text_layer_set_text(s_output_layer, s_buffer);
-        article one.title = t->value->cstring;
-        list.push(one);
-        
-        break;
-    }
-
-    // Get next pair, if any
-    t = dict_read_next(iterator);
-  }
-}
-
-static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
-}
-
-static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
-}
-
-static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
-}
-
-static void main_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect window_bounds = layer_get_bounds(window_layer);
-
-  // Create output TextLayer
-  s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
-  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(s_output_layer, "Waiting...");
-  text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
-  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
-}
-
-static void main_window_unload(Window *window) {
-  // Destroy output TextLayer
-  text_layer_destroy(s_output_layer);
-}
-
 static void init() {
-  // Register callbacks
-  app_message_register_inbox_received(inbox_received_callback);
-  app_message_register_inbox_dropped(inbox_dropped_callback);
-  app_message_register_outbox_failed(outbox_failed_callback);
-  app_message_register_outbox_sent(outbox_sent_callback);
-
-  // Open AppMessage
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-
-  // Create main Window
   s_main_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
-    .unload = main_window_unload
+    .unload = main_window_unload,
   });
   window_stack_push(s_main_window, true);
-
+  
+  printf("Before Article One");
+  
+  article one;
+  one.title = "Geisel";
+  one.content= "Janis";
+  printf("After Article One");
+  
+  article two;
+  two.title = "Radical";
+  two.content = "ChiCityMayne";
+  
+  printf("After Part Two");
+  
+  list[0] = one;
+  list[1] = two;
+  printf("After list assignment");
 }
 
 static void deinit() {
-  // Destroy main Window
   window_destroy(s_main_window);
 }
 
